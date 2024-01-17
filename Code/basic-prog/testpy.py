118af09fd30ac3d6fafdfc16fd3f96f9bc8ef850
Bancie <144613141+Bancie@users.noreply.github.com>
2024-01-17 13:16:37 +0700

    pip

---
+++ Code/basic-prog/testpy.py
@@ -0,0 +1,9 @@
+# Declare and initialize variables
+a, b = 5, 4
+# Post-increment a, then print a+b, a is now 6
+a += 1
+print(a + b)
+# a is already incremented, so print a+b again
+print(a + b)
+
+# Declare and initialize variables
