49b80446de9dde085b23e23e7bd9caff7ec1709c
Bancie <144613141+Bancie@users.noreply.github.com>
2024-08-26 11:17:41 +0700

    removed

--- app/tilearn/src/tilearn2.py
+++
@@ -1,5 +0,0 @@
-from tilearn.src.tilearn1 import plus
-
-def show(a, b):
-    c = plus(a, b)
-    return c
--- run.py
+++
@@ -1,7 +0,0 @@
-from tilearn.src.tilearn2 import (
-    show,
-)
-
-from tilearn.src.tilearn1 import (
-    plus,
-)
