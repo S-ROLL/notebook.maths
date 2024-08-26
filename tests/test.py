0485622a34c23ed14d0d68027db634d7e78291d3
Bancie <144613141+Bancie@users.noreply.github.com>
2024-08-26 11:35:11 +0700

    jupyter updated

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 import tilearn
 
-x = [[1, 3, 5, 4, 7], [2, 3, 6, 3, 7], [3, 6, 3, 6, 8], [4, 2, 1, 6, 3], [5, 3, 5, 4, 7], [6, 3, 6, 3, 7], [7, 6, 3, 6, 8], [8, 2, 1, 6, 3], [9, 10, 3, 6, 8]]
+x = [['French homework', 6, 3, 6, 8], ['Maths homework', 6, 3, 6, 8], ['IELTS practice', 2, 1, 6, 3], ['Review Maths', 3, 5, 4, 7], ['Study French', 3, 6, 3, 7], ['Housework', 3, 5, 4, 7], ['Coding practice', 2, 1, 6, 3], ['Reading book', 10, 3, 6, 8], ['Piano lesson', 3, 6, 3, 7], ['English homework', 3, 6, 3, 7]]
 
 # print(tilearn.show_mytime(x, 30))
 
